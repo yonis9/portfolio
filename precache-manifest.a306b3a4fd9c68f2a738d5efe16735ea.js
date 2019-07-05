2ce511fa7716768e180f797f27e9d7d790410372
Yoni Sisso <yonis9@walla.com>
2019-07-05 18:14:22 +0300

    Updates

---
+++ precache-manifest.a306b3a4fd9c68f2a738d5efe16735ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8ecaff97d45626b50b76d3353da006",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7274177e4942a44bd26f",
+    "url": "/portfolio/static/css/main.ec1d082d.chunk.css"
+  },
+  {
+    "revision": "6847c532bccb4f855425",
+    "url": "/portfolio/static/js/2.31e3bb36.chunk.js"
+  },
+  {
+    "revision": "7274177e4942a44bd26f",
+    "url": "/portfolio/static/js/main.add1e3ea.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b902c3cac967cebc4e699ff344c27eae",
+    "url": "/portfolio/static/media/Resume.b902c3ca.pdf"
+  }
+]);
