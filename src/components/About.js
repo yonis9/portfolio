dc3426970dbf5dda9325f9259a2a577835b6da9b
Yoni Sisso <yonis9@walla.com>
2020-02-02 20:53:51 +0200

    fixing grammer issues

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
                 <h1 data-aos="fade-right">ABOUT</h1>
                 <div className='border' data-aos="fade-right"></div>
                 <p><span data-aos="fade-right">I am a self-taught web developer from Israel. After discovering my passion for web development, I couldn’t get enough. </span>
-                <span data-aos="fade-right" data-aos-delay="100">I regularly create websites and web applications for my own portfolio, keep sharpening the saw, always thirst for more knowledge and look be up to date with technology .</span>
+                <span data-aos="fade-right" data-aos-delay="100">I regularly create websites and web applications for my own portfolio, keep sharpening the saw, always thirsty for more knowledge and look for being up to date with technology .</span>
                 <span data-aos="fade-right" data-aos-delay="200">I am looking forward to bringing that passion to a full-time role.
 </span></p>
             </div>
