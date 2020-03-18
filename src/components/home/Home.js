32987a5307d3959938fe2ce5094cca0a3978c216
Yoni Sisso <yonis9@walla.com>
2020-03-18 20:25:56 +0200

    changed particles style

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,37 +6,115 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const particleOptions = {
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800
+  "particles": {
+    "number": {
+      "value": 6,
+      "density": {
+        "enable": true,
+        "value_area": 400
+      }
+    },
+    "color": {
+      "value": "#1b1e34"
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 160,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "size_min": 40,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 200,
+      "color": "#ffffff",
+      "opacity": 1,
+      "width": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 8,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
         }
       },
-      color: {
-        value: '#95C623'
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
       },
-      size: {
-        value: 3
-    },
-      move: {
-        speed: 6
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
       }
-   },
-   interactivity: {
-    events: {
-    onhover: {
-    enable: true,
-    mode: "repulse"
-    },
-    onclick: {
-        enable: true,
-        mode: 'push'
     }
-    }
-    }
-  }
+  },
+  "retina_detect": true
+}
 
 const Home = ( { openNav, windowWidth } ) => {
     return (
@@ -50,8 +128,10 @@ const Home = ( { openNav, windowWidth } ) => {
             </div>
               : null    
                 }
-          <h1 className='tracking-in-expand'>Hi, I'm <span>Yoni.</span> </h1>
-          <p className='text-focus-in' id='desc'>A Full-Stack Web Developer</p> 
+          <div className='intro-container'>
+            <h1 className='tracking-in-expand'>Hi, I'm <span>Yoni.</span> </h1>
+            <p className='text-focus-in' id='desc'>A Full-Stack Web Developer</p> 
+          </div>
 
           <div className='slide-bottom'><NavLink
                smooth  to="/#about"
